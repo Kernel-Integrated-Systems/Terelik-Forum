37cefa698d03cfcbed769403f892f5b4e0862868
IVAYLO_GETSOV <ivaylo.getsov.a63@learn.telerikacademy.com>
2024-10-04 19:40:17 +0300

    added user models, services and routers

--- percistance/data.py
+++ percistance/data.py
@@ -1,4 +1,7 @@
 from modules.users import User
 
 
-users = [User(id=1, username='John', email='john@terelikacademy.com', password='12345', role='admin', is_active=1)]
+users = [
+    User(id=1, username='John', email='john@terelikacademy.com', password='12345', role='admin', is_active=1),
+    User(id=2, username='Maria', email='maria@terelikacademy.com', password='12345', role='user', is_active=1)
+]
