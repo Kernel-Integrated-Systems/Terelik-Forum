4f03c0ea43c2b4b21e9cd1c875f93cd99043a77f
SVP <S.petkov991@icloud.com>
2024-10-11 21:56:24 +0200

    basic connect

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
+
 from routers.router_topics import topics_router
-from routers.router_categories import categories_router
 from routers.router_users import users_router
 
 app = FastAPI()
-
+#
 app.include_router(topics_router)
-app.include_router(categories_router)
+# app.include_router(categories_router)
 app.include_router(users_router)
--- services/topic_services.py
+++ services/topic_services.py
@@ -1,30 +1,8 @@
-from modules.topics import Topics
-from percistance.data import topics
-from percistance.data import categories
+from modules import topics
+from percistance import data
 
-def create_topic(title: str, content: str, user_id: int, category: int):
-    new_id = max(topic.topic_id for topic in topics) + 1 if topics else 1
-    new_topic = Topics(topic_id=new_id, title=title, content=content, user_id=user_id, category_id=category)
-    topics.append(new_topic)
-    return new_topic
 
 
 def view_topics():
-    return topics
-
-
-def find_topic_by_id(id: int):
-    for topic in topics:
-        if topic.topic_id == id:
-            return topic
-
-
-def find_topic_by_title_f(title: str):
-    for topic in topics:
-        if title.lower() in topic.title.lower():
-            return topic
-
-
-def find_topic_by_category(category_id: int):
-    topic_l = [t for t in topics if t.category_id == category_id]
-    return topic_l
+    sql = '''SELECT topic_id, title, content, user_id, category_id FROM topics'''
+    return (topics.Topics.view_topics(*row) for row in data.read_query(sql))
