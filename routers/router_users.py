c1b08b3ed2aefa0ea6c5c3ceceaf5f4e83eae92b
Ivaylo Getsov <ivaylo.getsov.a63@learn.telerikacademy.com>
2024-10-16 11:38:26 +0300

    Update router_users.py
    Refactored users router

--- routers/router_users.py
+++ routers/router_users.py
@@ -1,37 +1,37 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response
 from modules.users import User, UserRegistrationRequest, UserLoginRequest
-from services.user_services import get_all_users
+from services.user_services import get_all_users, get_user_by_id, register_user, authenticate_user
 
-users_router = APIRouter(prefix='/users')
+users_router = APIRouter(prefix='/users', tags=['Users'])
 
 
 @users_router.get('/')
 def get_all_users_route():
-    return get_all_users()
+    try:
+        return get_all_users()
+    except ValueError as e:
+        return Response(content=str(e), status_code=400)
+
+@users_router.get('/{user_id}')
+def get_user_route(user_id: int):
+    try:
+        return get_user_by_id(user_id)
+    except ValueError as e:
+        return Response(content=str(e), status_code=400)
+
+@users_router.post('/register')
+def register_user_route(user: UserRegistrationRequest):
+    try:
+        return register_user(user.username, user.email, user.password)
+    except ValueError as err:
+        raise HTTPException(status_code=400, detail=str(err))
 
 
-# @users_router.get('/{user_id}')
-# def get_user_route(user_id: int):
-#     user = get_user_by_id(user_id)
-#     if not user:
-#         raise HTTPException(status_code=404, detail='User not found')
-#     return user
-#
-#
-# @users_router.post('/register')
-# def register_user_route(user: UserRegistrationRequest):
-#     try:
-#         new_user = register_user(user.username, user.email, user.password)
-#         return new_user
-#     except ValueError as err:
-#         raise HTTPException(status_code=400, detail=str(err))
-#
-#
-# @users_router.post('/login')
-# def login_user_route(user_login: UserLoginRequest):
-#     try:
-#         access_token = authenticate_user(**user_login.dict())
-#         return {"access token": access_token, "token_type": "bearer"}
-#     except ValueError as err:
-#         raise HTTPException(status_code=400, detail=str(err))
-#
+@users_router.post('/login')
+def login_user_route(user_login: UserLoginRequest):
+    try:
+        access_token = authenticate_user(**user_login.dict())
+        return {"access token": access_token, "token_type": "bearer"}
+    except ValueError as err:
+        raise HTTPException(status_code=400, detail=str(err))
+
