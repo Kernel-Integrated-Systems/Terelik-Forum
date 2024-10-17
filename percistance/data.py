f17537263f5263fa2c8278db27cdc47fc3c4d209
IVAYLO_GETSOV <ivaylo.getsov.a63@learn.telerikacademy.com>
2024-10-17 15:08:49 +0300

    Fixed data

--- percistance/data.py
+++ percistance/data.py
@@ -1,41 +1,37 @@
-import mariadb
+import base64
+from uuid import uuid4
+
+from fastapi import HTTPException
+
+session_store = {}
 
 
-
-def read_query(database: str, database_params=()):
-    with _conn_to_db() as conn:
-        cursor = conn.cursor()
-        cursor.execute(database, database_params)
-
-        return list(cursor)
+def authenticate(authorization) -> bool:
+    if not authorization and authorization == session_store["bearer"]:
+        raise HTTPException(status_code=401, detail="Authorization token missing or invalid")
+    return True
 
 
-def insert_query(database: str, database_params=()) -> int:
-    with _conn_to_db() as conn:
-        cursor = conn.cursor()
-        cursor.execute(database, database_params)
-        conn.commit()
-
-        return cursor.lastrowid
-#
-
-def update_query(database: str, database_params=()):
-    with _conn_to_db() as conn:
-        cursor = conn.cursor()
-        cursor.execute(database, database_params)
-        conn.commit()
-
-        return cursor.rowcount > 0
+def authorise_user_role(token: str):
+    user = session_store.get(token)
+    if not user:
+        raise ValueError("Invalid session or expired token")
+    _, username, user_role = decode(token)
+    return user_role
 
 
+def encode(user_id: int, username: str, user_role: int) -> str:
+    user_string = f"{user_id}_{username}_{user_role}"
+    encoded_bytes = base64.b64encode(user_string.encode('utf-8'))
 
+    return encoded_bytes.decode('utf-8')
 
+def decode(encoded_value: str):
+    decoded_string = base64.b64decode(encoded_value).decode('utf-8')
+    user_id, username, user_role = decoded_string.split('_')
 
-
-def _conn_to_db():
-    return mariadb.connect(
-        host="127.0.0.1",
-        port=3306,
-        user="root",
-        password="pass",
-        database='forum_api_db')
+    return {
+        "user_id": int(user_id),
+        "username": username,
+        "user_role": int(user_role)
+    }
