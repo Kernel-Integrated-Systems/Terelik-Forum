1337051a07a73ac4701991885d11e5cf8005f5d1
Kristin1805 <122818760+Kristin1805@users.noreply.github.com>
2024-11-01 13:57:21 +0200

    refix

--- modules/topic.py
+++ modules/topic.py
@@ -8,7 +8,7 @@ class Topics(BaseModel):
     content: str
     user_id: int
     category_id: int
-    is_locked: int = 0
+    is_locked: int
 
     @classmethod
     def from_query_string(cls, topic_id, title, content, user_id, category_id, is_locked):
